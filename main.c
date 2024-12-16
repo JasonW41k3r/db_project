15c62ba4494b8d3383c5af0e2fabac2c5dda5925
Jason Rao <noyer_rjs@163.com>
2024-12-16 21:42:06 +0800

    添加 MySQL 查询处理功能
    在代码中实现了对 MySQL 查询的处理，新增了 `MYSQL_RES* res` 和 `MYSQL_ROW* row` 变量以存储查询结果。将原有的 `mysql_query` 变量替换为 `query`，并在读取用户输入后执行查询。若查询执行失败，将输出相应的错误信息。同时，添加了循环以遍历查询结果并打印每一行的内容，确保在程序结束前关闭数据库连接并清理资源。

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 int main(void) {
 	mysql_library_init(0, NULL, NULL);
 	MYSQL* conn = mysql_init(NULL);
+	MYSQL_RES* res = NULL;
+	MYSQL_ROW* row = NULL;
+	
 
 	if (mysql_real_connect(conn, "127.0.0.1", "root", "", "routeplan", 0, NULL, NULL) == NULL) {
 		fprintf(stderr, "connect error! %s\n", mysql_error(conn));
@@ -14,10 +17,20 @@ int main(void) {
 	}
 
 	printf("Please enter your query: ");
-	char* mysql_query = calloc(100, sizeof(char));
-	fgets(mysql_query, 100, stdin);
-	mysql_query[strlen(mysql_query) - 1] = '\0';
+	char* query = calloc(100, sizeof(char)); // The longest query limited to 100 chars
+	fgets(query, 100, stdin);
+	query[strlen(query) - 1] = '\0';
 
+	if (mysql_query(conn, query)) {
+		fprintf(stderr, "Error while executing query! %s\n", mysql_error(conn));
+	}
+	res = mysql_use_result(conn);
+	while (row = mysql_fetch_row(res)) {
+		for (int cnt = 0; cnt < mysql_num_fields(res); cnt++) {
+			printf("%s\t", row[cnt] ? row[cnt] : "NULL");
+		}
+		printf("\n");
+	}
 
 	mysql_close(conn);
 	mysql_library_end();
